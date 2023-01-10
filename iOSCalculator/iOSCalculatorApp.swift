5f2f74920694803fce55599da684cef40d411492
VirajTank <virajtank98@gmail.com>
2023-01-10 06:50:10 +0530

    Initial Commit

---
+++ iOSCalculator/iOSCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSCalculatorApp.swift
+//  iOSCalculator
+//
+//  Created by Viraj Tank on 10/01/23.
+//
+
+import SwiftUI
+
+@main
+struct iOSCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

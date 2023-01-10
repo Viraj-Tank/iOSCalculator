b9c409d7188369ba9eb15b0a3dab5cced14a1a84
VirajTank <virajtank98@gmail.com>
2023-01-10 21:14:17 +0530

    setup enum and computation view

--- iOSCalculator/ComputationView.swift
+++ iOSCalculator/ComputationView.swift
@@ -7,16 +7,36 @@ struct ComputationView: View {
     let mainResult: String
     
     var body: some View {
-        VStack {
-            Text(currentComputation)
-            Text(mainResult)
-        }
+        VStack(spacing: 10) {
+            HStack {
+                Spacer()
+                Text(currentComputation)
+                    .foregroundColor(foregroundDigits)
+                    .lineLimit(1)
+            }.minimumScaleFactor(0.1)
+            // if our text go beyond the screen it will screen to 0.1 scalefactor if we dont add this it will put ...(three dot) at the end
+            
+            HStack {
+                Spacer()
+                Text(mainResult)
+                    .foregroundColor(foregroundDigits)
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .lineLimit(1)
+            }.minimumScaleFactor(0.1)
+        }.padding(.horizontal)
         
     }
 }
 
 struct ComputationView_Previews: PreviewProvider {
     static var previews: some View {
-        ComputationView(currentComputation: "5+1", mainResult: "6")
+        VStack {
+            Spacer()
+            ComputationView(currentComputation: "12000+936", mainResult: "12936")
+            Spacer()
+            ComputationView(currentComputation: "76%", mainResult: "0.76")
+            Spacer()
+        }
     }
 }
